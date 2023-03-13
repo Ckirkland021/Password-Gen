f31ffb9bfe6875a4cc1865155830228a93e13b73
Ckirkland021 <125106964+Ckirkland021@users.noreply.github.com>
2023-03-13 11:09:45 -0400

    Added some comments and also checked to see if the button is working by console log

--- assets/script.js
+++ assets/script.js
@@ -4,14 +4,31 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// CODE HERE
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
+
+
+// generate password function
+
+function generatePassword() {
+  console.log('button clicked');
+
+}
+
+// diplayy password on the screen
   passwordText.value = password;
 
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+// return string.fromCharCode(97);
+
