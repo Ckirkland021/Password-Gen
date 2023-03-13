962ef7c149137e5f80f5aeac7cf9783dea474427
Ckirkland021 <125106964+Ckirkland021@users.noreply.github.com>
2023-03-13 11:50:33 -0400

    added vars and confirms to diplay messages and set the vaules used for the password

--- assets/script.js
+++ assets/script.js
@@ -18,6 +18,31 @@ function writePassword() {
 function generatePassword() {
   console.log('button clicked');
 
+
+  //1. prompt the user for password criteria
+    prompt ('How many character would you like yout password to be?');
+    confirm ('Click OK to use upppercase letters??');
+    confirm ('Click OK to use lowerrcase letters??');
+    confirm ('Click OK to use special characters??');
+    confirm ('Click OK to use numbers??');
+  // Password length between 8 and 128
+  // 2. Prompt user as to if they want to use lowercase, uppercase, special and numbers
+
+
+  // 3. Validate the input
+  // 4. generate the password
+
+var lowercaseChar = 'abcdefghijklmnopqrstuvwxyz';
+var uppercaseChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+var numbersChar = '1234567890';
+var specChar = '!@#$%^&*()_+-={}|[]\:";<>?,./';
+
+
+
+
+
+// 5. Display the password on the page
+  return 'Generated Password';
 }
 
 // diplayy password on the screen
